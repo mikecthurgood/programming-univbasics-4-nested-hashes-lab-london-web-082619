b94add51de610e7a1c37108828970f9bffa0375c
Mike Thurgood <mikecthurgood@gmail.com>
2019-08-19 17:05:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,17 +85,8 @@ def adding_matz
         }
      }
      
-  matz = 
-    {
-        :yukihiro_matsumoto => {
-          :known_for => "Ruby",
-            :languages => ["LISP", "C"]
-        }
-    }
-    
-    programmer_hash.merge(matz)
-    
-    puts programmer_hash
+  programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["LISP, C"]}
+  
 end
 
 def changing_alan
